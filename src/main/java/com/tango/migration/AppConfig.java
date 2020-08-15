6f9424004c621c649e2172b89d0c59a5015fff1e
하재권 <hazxz@naver.com>
2020-08-15 18:28:47 +0900

    2-2. cacheManager 설정 전환

--- src/main/java/com/tango/migration/AppConfig.java
+++ src/main/java/com/tango/migration/AppConfig.java
@@ -1,9 +1,14 @@
 package com.tango.migration;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@EnableCaching
 @ComponentScan("com.tango.migration")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
@@ -15,5 +20,20 @@ public class AppConfig {
         placeholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
         placeholderConfigurer.setIgnoreResourceNotFound(true);
         return placeholderConfigurer;
+    }
+
+    @Bean
+    public EhCacheCacheManager cacheManager(EhCacheManagerFactoryBean factoryBean) {
+        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+        cacheManager.setCacheManager(factoryBean.getObject());
+        return cacheManager;
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehcache(ApplicationContext context) {
+        EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+        factoryBean.setConfigLocation(context.getResource("classpath:ehcache.xml"));
+        factoryBean.setShared(true);
+        return factoryBean;
     }
 }
