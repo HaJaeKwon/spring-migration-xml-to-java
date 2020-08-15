22935be4b35f5a774509b90de3d9b74021ee114f
하재권 <hazxz@naver.com>
2020-08-15 17:38:18 +0900

    1. applicationContext-config.xml 전환

---
+++ src/main/java/com/tango/migration/Config.java
@@ -0,0 +1,14 @@
+package com.tango.migration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public String userName(@Value("${user.name}") String userName) {
+        return userName;
+    }
+}
