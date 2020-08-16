d49ca557fbb07e5861b3823177bed2e1024a13ad
하재권 <hazxz@naver.com>
2020-08-16 11:47:32 +0900

    3. servlet-context.xml 전환

---
+++ src/main/java/com/tango/migration/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.tango.migration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.tango.migration")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogIntercepter())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
